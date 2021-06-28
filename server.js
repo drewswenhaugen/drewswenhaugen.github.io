86e60e33c7771863f1e30cdaa7c1bbb99646f04a
Travis Meyer <travis@foundrymakes.com>
2021-06-28 14:23:48 -0500

    Adds styles for individual pages

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("./"));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
